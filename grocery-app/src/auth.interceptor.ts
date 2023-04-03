10de93e5851058cf1ade6fd5f59a21c170579dc4
PHSolanki <priyanshusolanki23@gmail.com>
2023-04-03 09:33:19 +0530

    change password API works

---
+++ grocery-app/src/auth.interceptor.ts
@@ -0,0 +1,34 @@
+import { Injectable } from '@angular/core';
+import {
+  HttpRequest,
+  HttpHandler,
+  HttpEvent,
+  HttpInterceptor
+} from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { compileNgModule } from '@angular/compiler';
+
+@Injectable()
+export class AuthInterceptor implements HttpInterceptor {
+
+  constructor() {}
+
+  intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
+
+   let userToken = localStorage.getItem('token')
+   let token = userToken
+   console.log(token);
+
+   if(token){
+     let req = request.clone({
+      setHeaders:{
+        TOKEN  : token  
+     }
+    })
+    return next.handle(req)
+   }
+
+
+   return next.handle(request);
+  }
+}
